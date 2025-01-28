4bed1c397f0941f35d6e809a515769e58f96aa79
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-01-28 16:47:30 -0700

    Project Generation

---
+++ src/main/java/com/skycatdev/rlagents/RLAgents.java
@@ -0,0 +1,17 @@
+package com.skycatdev.rlagents;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RLAgents implements ModInitializer {
+	public static final String MOD_ID = "rl-agents";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
