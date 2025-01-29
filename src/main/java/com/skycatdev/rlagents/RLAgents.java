f17e6369784ec00cbbc052c51b572f96a27421ee
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-01-28 18:00:23 -0700

    Init gateway server

--- src/main/java/com/skycatdev/rlagents/RLAgents.java
+++ src/main/java/com/skycatdev/rlagents/RLAgents.java
@@ -4,11 +4,18 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import py4j.GatewayServer;
+
+import java.util.concurrent.Future;
 
 public class RLAgents implements ModInitializer {
 	public static final String MOD_ID = "rl-agents";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final GatewayServer GATEWAY_SERVER = new GatewayServer();
+
+	static {
+		GATEWAY_SERVER.start();
+	}
 
 	@Override
 	public void onInitialize() {
