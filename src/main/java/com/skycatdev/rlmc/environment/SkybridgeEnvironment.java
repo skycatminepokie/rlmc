466c0ff993dd10276bd91d2e66ef76069218ab22
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-02-01 22:11:33 -0700

    Fix and run spotless

--- src/main/java/com/skycatdev/rlmc/environment/SkybridgeEnvironment.java
+++ src/main/java/com/skycatdev/rlmc/environment/SkybridgeEnvironment.java
@@ -1,13 +1,11 @@
 /* Licensed MIT 2025 */
 package com.skycatdev.rlmc.environment;
 
+import carpet.fakes.ServerPlayerInterface;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import carpet.fakes.ServerPlayerInterface;
-import carpet.patches.EntityPlayerMPFake;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.ItemStack;
