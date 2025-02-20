30a866a74cbe7e7e2913db30cdc71d78e3131be7
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-02-20 16:58:41 -0700

    Maps of entity types, items, and block states
    See #1

--- src/main/java/com/skycatdev/rlmc/Rlmc.java
+++ src/main/java/com/skycatdev/rlmc/Rlmc.java
@@ -1,12 +1,23 @@
 /* Licensed MIT 2025 */
 package com.skycatdev.rlmc;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.skycatdev.rlmc.command.CommandManager;
 import com.skycatdev.rlmc.environment.Environment;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.state.StateManager;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import py4j.GatewayServer;
@@ -14,68 +25,114 @@ import py4j.GatewayServerListener;
 import py4j.Py4JServerConnection;
 
 public class Rlmc implements ModInitializer {
-	public static final String MOD_ID = "rl-agents";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	private static final GatewayServer GATEWAY_SERVER = new GatewayServer();
-	private static final List<Environment<?, ?>> ENVIRONMENTS = new ArrayList<>();
+    public static final String MOD_ID = "rl-agents";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private static final GatewayServer GATEWAY_SERVER = new GatewayServer();
+    private static final List<Environment<?, ?>> ENVIRONMENTS = new ArrayList<>();
+    private static @Nullable BiMap<EntityType<?>, Integer> ENTITY_TYPE_MAP = null;
+    private static @Nullable BiMap<Item, Integer> ITEM_MAP = null;
+    private static @Nullable BiMap<BlockState, Integer> BLOCK_STATE_MAP = null;
 
-	static {
-		new Thread(() -> GATEWAY_SERVER.start(false), "RLMC Python Gateway Server Thread").start();
+    static {
+        new Thread(() -> GATEWAY_SERVER.start(false), "RLMC Python Gateway Server Thread").start();
+    }
 
-	}
+    public static BiMap<BlockState, Integer> getBlockStateMap() {
+        if (BLOCK_STATE_MAP == null) {
+            List<BlockState> blockStates = Registries.BLOCK.stream()
+                    .filter(block -> Registries.BLOCK.getId(block).getNamespace().equals("minecraft"))
+                    .map(Block::getStateManager)
+                    .map(StateManager::getStates)
+                    .flatMap(Collection::stream)
+                    .toList();
+            BLOCK_STATE_MAP = HashBiMap.create(blockStates.size());
+            for (int i = 0; i < blockStates.size(); i++) {
+                BLOCK_STATE_MAP.put(blockStates.get(i), i);
+            }
+        }
+        return BLOCK_STATE_MAP;
+    }
 
-	@Override
-	public void onInitialize() {
-		CommandRegistrationCallback.EVENT.register(new CommandManager());
-		GATEWAY_SERVER.addListener(new GatewayServerListener() {
-			@Override
-			public void connectionError(Exception e) {
-				LOGGER.debug("Py4J Connection Error. Printing stack trace.", e);
-			}
+    public static BiMap<EntityType<?>, Integer> getEntityTypeMap() {
+        if (ENTITY_TYPE_MAP == null) {
+            List<EntityType<?>> entityTypes = Registries.ENTITY_TYPE.stream()
+                    .filter(type -> Registries.ENTITY_TYPE.getId(type).getNamespace().equals("minecraft"))
+                    .sorted(Comparator.comparing(o -> Registries.ENTITY_TYPE.getId(o).getPath()))
+                    .toList();
+            ENTITY_TYPE_MAP = HashBiMap.create(entityTypes.size());
+            for (int i = 0; i < entityTypes.size(); i++) {
+                ENTITY_TYPE_MAP.put(entityTypes.get(i), i);
+            }
+        }
+        return ENTITY_TYPE_MAP;
+    }
 
-			@Override
-			public void connectionStarted(Py4JServerConnection gatewayConnection) {
-				LOGGER.debug("Py4J connection started");
-			}
+    public static List<Environment<?, ?>> getEnvironments() {
+        return ENVIRONMENTS;
+    }
 
-			@Override
-			public void connectionStopped(Py4JServerConnection gatewayConnection) {
-				LOGGER.debug("Py4J connection stopped");
-			}
+    public static BiMap<Item, Integer> getItemMap() {
+        if (ITEM_MAP == null) {
+            List<Item> items = Registries.ITEM.stream()
+                    .filter(item -> Registries.ITEM.getId(item).getNamespace().equals("minecraft"))
+                    .sorted(Comparator.comparing(o -> Registries.ITEM.getId(o).getPath()))
+                    .toList();
+            ITEM_MAP = HashBiMap.create(items.size());
+            for (int i = 0; i < items.size(); i++) {
+                ITEM_MAP.put(items.get(i), i);
+            }
+        }
+        return ITEM_MAP;
+    }
 
-			@Override
-			public void serverError(Exception e) {
-				LOGGER.debug("Py4J Server Error. Print stack trace.", e);
-			}
+    public static PythonEntrypoint getPythonEntrypoint() {
+        return (PythonEntrypoint) GATEWAY_SERVER.getPythonServerEntryPoint(new Class[]{PythonEntrypoint.class});
+    }
 
-			@Override
-			public void serverPostShutdown() {
-				LOGGER.debug("Py4J server post shutdown");
-			}
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register(new CommandManager());
+        GATEWAY_SERVER.addListener(new GatewayServerListener() {
+            @Override
+            public void connectionError(Exception e) {
+                LOGGER.debug("Py4J Connection Error. Printing stack trace.", e);
+            }
 
-			@Override
-			public void serverPreShutdown() {
-				LOGGER.debug("Py4J server pre shutdown");
-			}
+            @Override
+            public void connectionStarted(Py4JServerConnection gatewayConnection) {
+                LOGGER.debug("Py4J connection started");
+            }
 
-			@Override
-			public void serverStarted() {
-				LOGGER.debug("Py4J server started");
-			}
+            @Override
+            public void connectionStopped(Py4JServerConnection gatewayConnection) {
+                LOGGER.debug("Py4J connection stopped");
+            }
 
-			@Override
-			public void serverStopped() {
-				LOGGER.debug("Py4J server stopped");
-			}
-		});
-	}
+            @Override
+            public void serverError(Exception e) {
+                LOGGER.debug("Py4J Server Error. Print stack trace.", e);
+            }
 
-	public static PythonEntrypoint getPythonEntrypoint() {
-		return (PythonEntrypoint) GATEWAY_SERVER.getPythonServerEntryPoint(new Class[]{PythonEntrypoint.class});
-	}
+            @Override
+            public void serverPostShutdown() {
+                LOGGER.debug("Py4J server post shutdown");
+            }
 
-	public static List<Environment<?,?>> getEnvironments() {
-		return ENVIRONMENTS;
-	}
+            @Override
+            public void serverPreShutdown() {
+                LOGGER.debug("Py4J server pre shutdown");
+            }
+
+            @Override
+            public void serverStarted() {
+                LOGGER.debug("Py4J server started");
+            }
+
+            @Override
+            public void serverStopped() {
+                LOGGER.debug("Py4J server stopped");
+            }
+        });
+    }
 
 }
