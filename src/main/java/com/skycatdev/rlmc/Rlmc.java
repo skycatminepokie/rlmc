46e38a6f61fb79ddea1308b74946d8cad5581a2f
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-03-06 14:43:57 -0700

    Fix modid

--- src/main/java/com/skycatdev/rlmc/Rlmc.java
+++ src/main/java/com/skycatdev/rlmc/Rlmc.java
@@ -24,7 +24,7 @@ import py4j.GatewayServerListener;
 import py4j.Py4JServerConnection;
 
 public class Rlmc implements ModInitializer {
-    public static final String MOD_ID = "rl-agents";
+    public static final String MOD_ID = "rlmc";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     private static final GatewayServer GATEWAY_SERVER = new GatewayServer();
     /**
